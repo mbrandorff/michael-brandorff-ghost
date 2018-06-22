63371e3d7cbac18d06d571cd6978a45a7545ad1e
Michael <michi.brandorff@gmail.com>
2018-06-22 16:36:33 +0200

    not opening map links in new tab anymore

--- assets/js/mapdata.js
+++ assets/js/mapdata.js
@@ -59,7 +59,7 @@ var simplemaps_worldmap_mapdata={
     //Advanced settings
     div: "map",
     auto_load: "yes",
-    url_new_tab: "yes",
+    url_new_tab: "no",
     images_directory: "default",
     fade_time: 0.1,
     link_text: "View Website",
@@ -249,7 +249,8 @@ var simplemaps_worldmap_mapdata={
     },
     EG: {
       inactive: "no",
-      name: "Egypt"
+      name: "Egypt",
+      color: "#FFD228"
     },
     ER: {
       inactive: "no",
